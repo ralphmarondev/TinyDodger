1dce043939fbc8235c729a4e8922c229a0608ad5
Ralph Maron A. Eda <edaralphmaron@gmail.com>
2026-05-26 09:27:22 +0800

    move player with keyboard input

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,10 +2,19 @@ class_name Player extends CharacterBody2D
 
 var speed := 300
 
-func _ready() -> void:
-	pass
-	
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var direction = Vector2(0, 0)
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x = 1
+	if Input.is_action_pressed("move_left"):
+		direction.x = -1
+	if Input.is_action_pressed("move_up"):
+		direction.y = -1
+		# In godot going up is negative. Wierd right? HAHAHAH
+	if Input.is_action_pressed("move_down"):
+		direction.y = 1
+		
+	direction = direction.normalized()
 	velocity = direction * speed
 	move_and_slide()
